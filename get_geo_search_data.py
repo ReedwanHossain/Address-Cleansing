d3574155e00be1c40ce7e79feb8dfb552ed0d8b1
rafi138 <rafiqul.abprs@gmail.com>
2021-07-26 18:19:40 +0600

    subdistrict spatial added

--- get_geo_search_data.py
+++ get_geo_search_data.py
@@ -36,8 +36,7 @@ def get_db_data(item, tablename, field, field_val):
     insertcursor = mydb.cursor()
     iteratorcursor = mydb.cursor(buffered=True)
     querycursor = mydb.cursor()
-    querycursor.execute(
-        "SELECT "+item+" from " + tablename + " where "+tablename+"."+field+"="+repr(field_val) + ";")
+    querycursor.execute("SELECT "+item+" from " + tablename + " where "+tablename+"."+field+"="+repr(field_val) + ";")
     count = 0
     gotAddress = querycursor.fetchall()
     return gotAddress
@@ -66,30 +65,103 @@ def get_dsu_comp(addr):
 
 def get_geo_data(raw_input_addr,q):
     data=[]
+    comp={'district':None,'sub_district':None,'union':None}
     try:
         comp=get_dsu_comp(raw_input_addr)[0]
-        print(q)
-        print(comp)
-        district=comp['district']
-        print(district)
-        if district!=None:
-            data=get_data_by_polygon_search(q,district)
-        #print(len(data))
-        if len(data)>0:
-            return data
-    except Exception as e:
-        print(e.with_traceback)
+    except:
+        pass
+    try:
+        if comp['union']!=None:
+            polygon = get_db_data_union(comp['district'], comp['union'])
+            # print(str(polygon.split()))
+            data = polygon_search(q, str(polygon))
+            print('data from union boundary')
+    except:
         pass
 
-
-    url = 'https://admin.barikoi.xyz:8090/v2/search/autocomplete/web?q=' + q
     try:
-        data= requests.get(url).json()['places']
-    except Exception as e:
-        print(e)
-    print(data)
+        if comp['sub_district']!=None and len(data)==0:
+            polygon = get_db_data_subdis(comp['district'], comp['sub_district'])[0][0]
+            # print(str(polygon.split()))
+            data = polygon_search(q, str(polygon.replace('(','').split()))
+            print('data from subdistrict boundary')
+    except:
+        pass
+    
+    try:
+        if comp['district']!=None and len(data)==0:
+            data=get_data_by_polygon_search(q,comp['district'])
+            print('data from district boundary')
+    except:
+        pass
+    
+    if len(data)==0:
+
+        url = 'https://admin.barikoi.xyz:8090/v2/search/autocomplete/web?q=' + q
+        try:
+            data= requests.get(url).json()['places']
+        except Exception as e:
+            print(e)
     return data
 
+
+def get_db_data_union(district, union):
+    data = []
+    mydb=getdb('db.barikoi.com', 'barikoiadmin','Amitayef5.7', 'ethikana')
+    item = 'bounds'
+    tablename = 'unions'
+    tablename2 = 'areas_fixed'
+    # try:
+    #     mydb = getdb('db.barikoi.com', 'barikoiadmin',
+    #                  'Amitayef5.7', 'ethikana')
+    #     print("Sucessfully connected with ethikana !")
+    #     print('Fetching data from '+tablename+' Please wait..')
+    # except Exception as e:
+    #     print(e)
+    #     print('from get_db_data_union')
+    insertcursor = mydb.cursor()
+    iteratorcursor = mydb.cursor(buffered=True)
+    querycursor = mydb.cursor()
+    querycursor.execute("SELECT unions.bounds from unions inner join subdistricts ON unions.subdistrict_id=subdistricts.id where subdistricts.adm2_en="+repr(district)+" and unions.adm4_en="+repr(union)+";")
+    count = 0
+    polygon = []
+    gotAddress = querycursor.fetchall()
+    #print(gotAddress[0][0])
+    try:
+        pol_str=gotAddress[0][0].replace('[[[','[')
+        pol_str=pol_str.replace(']]]',']')
+        data = eval(pol_str)
+        #print(data)
+        for i in data:
+            polygon.append(str(i[1]) + ',' + str(i[0]))
+    except Exception as e:
+        print('from get_db_data_union')
+        print(e)
+        pass
+    # print(polygon)
+    return polygon
+
+def get_db_data_subdis(district, subdistrict):
+    data = []
+    item = 'bounds'
+    tablename = 'subdistricts'
+    mydb=getdb('db.barikoi.com', 'barikoiadmin','Amitayef5.7', 'ethikana')
+    # try:
+    #     mydb = getdb('db.barikoi.com', 'barikoiadmin',
+    #                  'Amitayef5.7', 'ethikana')
+    #     print("Sucessfully connected with ethikana !")
+    #     print('Fetching data from '+tablename+' Please wait..')
+    # except Exception as e:
+    #     print(e)
+    #     print('from get_db_data_subdis')
+    insertcursor = mydb.cursor()
+    iteratorcursor = mydb.cursor(buffered=True)
+    querycursor = mydb.cursor()
+    querycursor.execute(
+        "SELECT "+item+" from " + tablename + " where "+tablename+".adm2_en="+repr(district)+" and "+tablename+".adm3_en="+repr(subdistrict)+";")
+    count = 0
+    gotAddress = querycursor.fetchall()
+    return gotAddress
 
 if __name__ == "__main__":
     print(get_geo_data('mirpur'))
